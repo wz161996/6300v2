c65c95eebfc545abe5266034b83137ff01ce54d6
wz161996 <wz161996@foxmail.com>
2017-12-30 11:07:55 +0800

    11

--- tmp_hsfq_update.sh
+++ tmp_hsfq_update.sh
@@ -5,7 +5,8 @@ LOGTIME=$(date "+%m-%d %H:%M:%S")
 
 echo " 检测安装脚本版本 "
 if [ ! -f /tmp/hsfq_script_up.sh ]; then
-	wget --no-check-certificate https://github.com/wz161996/6300v2/blob/master/hsfq_script.sh -O /tmp/hsfq_script_up.sh;chmod 775 /tmp/hsfq_script_up.sh
+	wget --no-check-certificate https://github.com/wz161996/6300v2/blob/master/hsfq_script.sh -O /tmp/hsfq_script_up.sh
+	chmod 775 /tmp/hsfq_script_up.sh
 	cat /tmp/hsfq_script_up.sh /jffs/configs/dnsmasq.d/hsfq_script.sh | awk '{ print$0}' | sort | uniq -u > /tmp/hsfq_script_up.txt && sleep 2
 	if [ ! -s "/tmp/hsfq_script_up.txt" ]; then
 		echo -e "\e[1;33m HSFQ 安装脚本已为最新,无需更新.\e[0m\n" && rm -f /tmp/hsfq_script_up.sh && rm -f /tmp/hsfq_script_up.txt
@@ -30,5 +31,5 @@ if [ -x "/jffs/configs/dnsmasq.d/hsfq_update.sh" ]; then
 	wget --no-check-certificate -t 10 -T 30 https://github.com/wz161996/6300v2/blob/master/tmp_hs_up -qO /tmp/tmp_hs_up && chmod 755 /tmp/tmp_hs_up && . /tmp/tmp_hs_up
 	restart_dhcpd && /usr/sbin/dnsmasq restart 2>&1 >/dev/null
 fi
-sleep 3 && exit 0
+sleep 3s && exit 0
 
